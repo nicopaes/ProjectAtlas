cfe9ae783738fe6bc248c3aa93d63195544f4e4f
Nicolas P Leme <nicolaspleme@gmail.com>
2018-07-04 21:39:18 -0300

    + Mouse pointer

---
+++ Assets/FollowMouse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour 
+{
+	void OnEnable()
+	{
+		Cursor.visible = false;
+	}
+	void Update () 
+	{
+		GetComponent<RectTransform>().position = Input.mousePosition - new Vector3(1,1,0);
+	}
+}
