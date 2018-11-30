710dd1eef107fd234bb7a698de92fa59fb9e0fdd
Nicolas Paes Leme <nickxp51@hotmail.com>
2018-11-30 15:30:17 -0200

    Oculos Go Features

---
+++ Assets/OculusGoInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OculusGoInput : MonoBehaviour
+{
+    public LayerMask lm;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Debug.DrawRay(transform.position, transform.forward, Color.red);
+        Ray newRay = new Ray(transform.position, transform.forward);
+        OVRInput.Update();
+
+        
+        RaycastHit info = new RaycastHit();
+        if (Physics.Raycast(newRay, out info, Mathf.Infinity, lm))
+        {
+            ChangeSceneTarget cst = info.transform.GetComponent<ChangeSceneTarget>();
+            if (cst != null)
+            {
+                Debug.Log(" Hello");
+                GetComponent<LineRenderer>().startColor = Color.green;
+                GetComponent<LineRenderer>().endColor = Color.green;
+                if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+                {
+                    GetComponent<LineRenderer>().startColor = Color.red;
+                    GetComponent<LineRenderer>().endColor = Color.red;
+                    cst.Action();
+                }
+            }
+        }
+        else
+        {
+            GetComponent<LineRenderer>().startColor = Color.white;
+            GetComponent<LineRenderer>().endColor = Color.white;
+        }
+    }
+}
